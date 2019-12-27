a2f1f1429a7f1e65e51223502480739a12a28fa6
CodyVollrath <950doenut.buff@gmail.com>
2019-12-27 01:11:47 -0500

    git commit

--- Family-Tree/JS/familyData.js
+++ Family-Tree/JS/familyData.js
@@ -50,6 +50,38 @@ var family = new Vue({
                 birthday:'06/28/1974',
                 bio:'Mother of one - Karis Smith',
                 image:'IMAGES/Sandy.jpg'
+            },
+            Donald:{
+                id:5,
+                name:"Donald Smith",
+                age:getAge('07/31/1968'),
+                birthday:'07/31/1968',
+                bio:'Brother, despite everything, we will always love him. He made poor decisions but it is important to remmber that we can forgive him when he seeks forgivness. Do not let his worst actions define him. People can change, and if we are willing to let them do it and accept them when they do, then that change can be permanent. Forgivness can only be given to those who seek it, but if they should seek it, we should consider the path forward.',
+                image:'IMAGES/Donald.jpg'
+            },
+            Matt:{
+                id:6,
+                name:"Matthew Smith",
+                age:getAge('08/01/1970'),
+                birthday:'08/01/1970',
+                bio:"Borther",
+                image:'IMAGES/Matt.jpg'
+            },
+            Joe:{
+                id:7,
+                name:"Joseph Smith",
+                age:getAge('01/24/1973'),
+                birthday:'01/24/1973',
+                bio:"Brother, and a crazy one",
+                image:'IMAGES/Joe.jpg'
+            },
+            Tracy:{
+                id:8,
+                name:"Tracy Vollrath",
+                age:getAge('09/30/1974'),
+                birthday:'09/30/1974',
+                bio:"Another brother, another crazy one",
+                image:'IMAGES/Tracy.jpg'
             }
         }
     },
